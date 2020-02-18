a9b55baebb1b246eefe9c0287c355851a6e5fe2e
Srihita <srihitaramini@gmail.com>
2020-02-17 17:08:51 -0800

    Initial Commit

---
+++ farmer2.0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  farmer2.0
+//
+//  Created by Srihita on 2/17/20.
+//  Copyright © 2020 Srihita. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
